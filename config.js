dfc580a4853bde1085e0d46293d5ab6155549887
Luke Isborne <isbornelouie0@gmail.com>
2026-04-28 02:03:51 +0100

    Added

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ const PRODUCTS = [
     author:      "JD Development & Designs",
     downloadUrl: "https://drive.google.com/drive/folders/17_HLSJvH4Qa-EI0KF6jDA9ezB6ejgQ52?usp=sharing",
     description: "A simple loading screen for your FiveM server with smooth image loop transitions.",
-    image:       "https://cdn.discordapp.com/attachments/1498246763893751878/1498247097353240686/image.png?ex=69f076d8&is=69ef2558&hm=90e0779b427dc638589b6f9294797b613571acb36016d373e05c79bd2b4ca2cf&"
+    image: "https://cdn.discordapp.com/attachments/1498246763893751878/1498247097353240686/image.png?ex=69f076d8&is=69ef2558&hm=90e0779b427dc638589b6f9294797b613571acb36016d373e05c79bd2b4ca2cf&"
   },
   {
     id:          "cppd-liveries",
@@ -52,7 +52,7 @@ const PRODUCTS = [
     author:      "JD Development & Designs",
     downloadUrl: "https://drive.google.com/drive/folders/18f7sO3F2u_bkpCyWbRPVdTPRBUD7wbmL?usp=drive_link",
     description: "A simple livery package for Cayo Perico Police Department vehicles.",
-    image:       "https://media.discordapp.net/attachments/1372494424931700787/1415474784422199357/Screenshot_2025-09-11_000257.png?ex=69f13e9e&is=69efed1e&hm=a106bf2be51d2181e4eef1a3cf4a916d163dc4ccb939fe3896961402cb718d07&animated=true"
+    image: "https://media.discordapp.net/attachments/1372494424931700787/1415474784422199357/Screenshot_2025-09-11_000257.png?ex=69f13e9e&is=69efed1e&hm=a106bf2be51d2181e4eef1a3cf4a916d163dc4ccb939fe3896961402cb718d07&animated=true"
   },
   {
     id:          "whitelist-eup",
@@ -65,6 +65,6 @@ const PRODUCTS = [
     author:      "JD Development & Designs",
     downloadUrl: "https://drive.google.com/drive/folders/18f7sO3F2u_bkpCyWbRPVdTPRBUD7wbmL?usp=drive_link",
     description: "GUIDE: Once installed and configured, you can run the command /whitelisteup and scroll through the list of EUP components to select which one you want to restrict. You can then restrict it via Discord role ID. You will need a Discord bot for this script to function properly, as it checks Discord roles to enable locking.\nAce Perm: command.whitelisteup",
-    image: ""
+    image: null
   }
 ];
